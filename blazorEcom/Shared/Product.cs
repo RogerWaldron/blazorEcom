060bf2da1bed44ee5cc6a0bfda669cca11c8abbf
rwaldron <outsideinprofits@gmail.com>
2023-09-13 23:47:33 +1000

    Added Product model

---
+++ blazorEcom/Shared/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace blazorEcom.Shared
+{
+    public class Product
+	{
+		public Product()
+        {
+			public int Id { get; set; }
+
+			public string Title { get; set; } = string.Empty;
+
+			public string Description { get; set; } = string.Empty;
+
+			public string ImageUrl { get; set; } = string.Empty;
+
+			public decimal Price { get; set; }
+		}
+	}
+}
+

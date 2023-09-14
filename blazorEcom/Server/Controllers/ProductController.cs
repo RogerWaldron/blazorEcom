4ff89dcab30777ffbef1db3e82bcc687e1d45d3d
Roger Waldron <87526672+RogerWaldron@users.noreply.github.com>
2023-09-14 07:57:29 +0400

    Product skeleton (#1)
    * Added Product model
    
    * Added using blazorEcom.Shared
    
    * Added Product Model
    
    * Added ProductList Component
    
    * Added dependency Microsoft.VisualStudio.Web.CodeGeneration.Design
    
    Need to scaffold out items
    
    * Added Dependency Swagger
    
    * Added Dependencies EntityFrameworkCore and SQL Server
    
    * Added DataContext
    
    * Added Initial ProductController GetProducts()
    
    * Added Column annotation to Price decimal field
    
    * Added Initial Migration
    
    * Added SQL Connection String and registered DbContext
    
    * Fixed typo in Product model, remove migration and recreated
    
    * Refactored Seed data out of GetProducts into DataContext OnModelCreate()
    
    * Added migration SeedProduct
    
    * Fix typo missing [] in route annotation
    
    ---------
    
    Co-authored-by: rwaldron <outsideinprofits@gmail.com>

---
+++ blazorEcom/Server/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using blazorEcom.Server.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace blazorEcom.Server.Controllers
+{
+    [Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+
+        private static readonly List<Product> _products = new();
+        private readonly DataContext _dataContext;
+
+        public ProductController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+ 
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _dataContext.Products.ToListAsync();
+            return Ok(products);
+        }
+	}
+}
+
--- blazorEcom/Server/Program.cs
+++ blazorEcom/Server/Program.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.ResponseCompression;
+﻿global using blazorEcom.Shared;
+using blazorEcom.Server.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,6 +29,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
 
 app.UseHttpsRedirection();
 

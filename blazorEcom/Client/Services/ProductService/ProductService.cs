302b1fb6087e65c763d78d2dff71409c13500378
rwaldron <outsideinprofits@gmail.com>
2023-09-14 16:07:53 +1000

    Added Client ProductService

---
+++ blazorEcom/Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Json;
+using blazorEcom.Shared;
+
+namespace blazorEcom.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+	{
+        private readonly HttpClient _httpClient;
+
+
+        public ProductService(HttpClient httpClient)
+		{
+            _httpClient = httpClient;
+        }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public async Task GetProducts()
+        {
+            var results = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+
+            if(results != null && results.Data != null)
+                Products = results.Data;
+        }
+    }
+}
+

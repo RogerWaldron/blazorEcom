af863ea81eccd6227b700a96660bd4ae7d05715e
rwaldron <outsideinprofits@gmail.com>
2023-09-14 13:47:51 +1000

    Fix typo missing [] in route annotation

--- blazorEcom/Server/Controllers/ProductController.cs
+++ blazorEcom/Server/Controllers/ProductController.cs
@@ -1,20 +1,28 @@
-﻿using System;
+﻿using blazorEcom.Server.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace blazorEcom.Server.Controllers
 {
-	[Route("api/controller")]
+    [Route("api/[controller]")]
 	[ApiController]
 	public class ProductController : ControllerBase
 	{
 
-        private static List<Product> Products = new();
+        private static readonly List<Product> _products = new();
+        private readonly DataContext _dataContext;
+
+        public ProductController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
  
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return Ok(Products);
+            var products = await _dataContext.Products.ToListAsync();
+            return Ok(products);
         }
 	}
 }

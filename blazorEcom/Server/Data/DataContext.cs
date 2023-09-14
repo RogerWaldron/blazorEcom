ff927edcc260ce04eb2e0babcf1cdbb950c1a78c
rwaldron <outsideinprofits@gmail.com>
2023-09-14 11:54:08 +1000

    Added DataContext

---
+++ blazorEcom/Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace blazorEcom.Server.Data
+{
+    public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
+

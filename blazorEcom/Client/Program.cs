a5baa8c34f8c38662b356c0a0abc909cf7191197
rwaldron <outsideinprofits@gmail.com>
2023-09-16 11:31:49 +1000

    Added Client Category Service

--- blazorEcom/Client/Program.cs
+++ blazorEcom/Client/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using blazorEcom.Client;
 using blazorEcom.Client.Services.ProductService;
+using blazorEcom.Client.Services.CategoryService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +10,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
 
 await builder.Build().RunAsync();
 
